774ccb569463dbfc65b79568c4d0f26c1e9e9aa0
Chris Mytton <self@hecticjeff.net>
2013-07-17 00:31:33 +0100

    Fix flag help text

--- httpserver.go
+++ httpserver.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-var port = flag.String("port", "8080", "Define what port TCP port to bind to")
+var port = flag.String("port", "8080", "Define what TCP port to bind to")
 var root = flag.String("root", ".", "Define the root filesystem path")
 
 func main() {
