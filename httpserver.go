aa8c8afb114b2b72828056f4d47c2772378bd3c1
Chris Mytton <self@hecticjeff.net>
2013-07-16 23:05:09 +0100

    Take arguments on the command line

--- httpserver.go
+++ httpserver.go
@@ -1,8 +1,14 @@
 package main
+
 import (
+	"flag"
 	"net/http"
-	"os"
 )
+
+var port = flag.String("port", "8080", "Define what port TCP port to bind to")
+var root = flag.String("root", ".", "Define the root filesystem path")
+
 func main() {
-	panic(http.ListenAndServe(":"+os.Getenv("PORT"), http.FileServer(http.Dir("."))))
+	flag.Parse()
+	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*root))))
 }
